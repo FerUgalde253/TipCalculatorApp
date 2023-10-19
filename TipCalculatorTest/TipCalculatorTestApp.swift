8f837b8b157fc88869c576df8f944c8e318c9201
Fer Ugalde <fernando.ugalde.mtz@gmail.com>
2023-10-18 22:07:27 -0600

    Test of correct result and test of nil result was adde

---
+++ TipCalculatorTest/TipCalculatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorTestApp.swift
+//  TipCalculatorTest
+//
+//  Created by Fernando Ugalde on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipCalculatorTestTests/TipCalculatorTestTests.swift
@@ -0,0 +1,35 @@
+//
+//  TipCalculatorTestTests.swift
+//  TipCalculatorTestTests
+//
+//  Created by Fernando Ugalde on 18/10/23.
+//
+
+import XCTest
+@testable import TipCalculatorTest
+
+final class TipCalculatorTestTests: XCTestCase {
+    func testTipCalculation_WhenATipAndAmoutAreGiven_ShouldPass(){
+        //Given
+        let amountBill = 1500.0
+        let tipPercentage = 15.0
+        let sut = Calculation()
+        let expectedResult = 225.0
+        //When
+        let tip1 = sut.calculateTip(of: amountBill, with: tipPercentage)
+        //Assert
+        XCTAssertEqual(tip1, expectedResult)
+    }
+   
+    func testTipCalculationNil_WhenReciveANegativeAmout_ShouldntPass(){
+        //Given
+        let amountBill = -1500.0
+        let tipPercentage = 15.0
+        let sut = Calculation()
+        
+        //When
+        let tip1 = sut.calculateTip(of: amountBill, with: tipPercentage)
+        //Assert
+        XCTAssertNil(tip1)
+    }
+}
